12776180abb44bcd2ac7592643c91cbc062683ab
M307users <m307user@m307.uc-bcf.edu.ph>
2022-09-15 11:24:42 +0800

    upload git

---
+++ git1.php
@@ -0,0 +1,11 @@
+        <?php
+        
+        echo "\n  Twinkle, Twinkle little Star! \n ";
+        
+        $twinkle = "Twinkle";
+        $star ="Star";
+        
+        echo " $twinkle, $twinkle little $star! ";
+        
+        ?>
+ 
