f28d6c6422daaf35813b55e4892250f0fd391a55
M307users <m307user@m307.uc-bcf.edu.ph>
2022-09-15 11:25:13 +0800

    add git 2

---
+++ git2.php
@@ -0,0 +1,21 @@
+
+ 
+        <?php
+        
+        echo "\n  \n ";
+        
+        $x=10;
+        $y=7;
+        
+        $sum = $x +  $y;
+        $diff = $x -  $y;
+        $prod = $x *  $y;
+        $div = $x /  $y;
+        $mod = $x % $y;
+         echo "$x + $y =  $sum\n";
+         echo "$x - $y =  $diff\n";
+         echo "$x X $y =  $prod\n";
+         echo "$x X $y = $div\n";
+         echo "$x % $y =  $mod ";
+ ?>
+ 
