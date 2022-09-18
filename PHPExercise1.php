faebc184a738a980273c2637730194fe3fb9969a
JustinBryan69 <jtin253@gmail.com>
2022-09-18 22:48:13 +0800

    PHP Exercise 1 upload

---
+++ PHPExercise1.php
@@ -0,0 +1,18 @@
+
+
+ 
+  <?php
+        
+        echo "\n  Twinkle, Twinkle little Star! \n ";
+        
+        $twinkle = "Twinkle";
+        $star ="Star";
+        
+         echo " $twinkle, $twinkle little $star! ";
+         $twinkle = "twinkle twinkle";
+         $star = "star star";
+
+          echo "\n";
+          echo " $twinkle, $twinkle little $star! ";
+        ?>
+	
