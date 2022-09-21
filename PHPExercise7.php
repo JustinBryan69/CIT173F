bf129e3bbecada1068fcec36f5c19d572cefee38
JustinBryan69 <jtin253@gmail.com>
2022-09-21 10:57:28 +0800

    PHP Exercise 7 upload

---
+++ PHPExercise7.php
@@ -0,0 +1,10 @@
+
+<?php
+
+  for ( $counter1 = 1; $counter1 <= 7; $counter1++ ) {
+  echo " \n";
+    for ( $counter2 = 1; $counter2 <= 7; $counter2++)
+      echo "  ".$counter1 * $counter2."  ";			
+  echo "\n";
+  }
+?>
